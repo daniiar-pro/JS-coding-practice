e3bab8b73b319ab8e2533a4c857ce8c40db92b02
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-02-22 10:55:50 +0100

    some questions for easy part

---
+++ js-coding-practice-easy/script_1_.js
@@ -0,0 +1,29 @@
+// 1) print
+
+// Your function will be given an input parameter incoming. Please console.log this value.
+
+// `print('test') // test`
+
+// SOLUTION: 
+
+// ### function test(){
+    // console.log('test')
+// };
+// test();
+
+
+// ================================================================>
+
+// 2) sayHello
+
+// Create a function sayHello(). Within it, console.log string "Hello World!"
+
+// SOLUTION:
+
+//  function sayHello(){
+//     console.log('Hello World!');
+//  }
+//  sayHello();
+
+// ======================================================================>
+
