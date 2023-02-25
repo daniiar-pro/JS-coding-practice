ddccc01775c29eca2965a46b0f5443ea5f5de298
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-02-25 13:28:40 +0100

    added daily dose of coding practice

--- js-coding-practice-easy/script_2_.js
+++ js-coding-practice-easy/script_2_.js
@@ -96,3 +96,83 @@
 //   console.log(isOldEnoughToVote(17)); // false
 //   console.log(isOldEnoughToVote(18)); // true
 
+// ====================================================>
+
+// 16)  isOldEnoughToDrinkAndDrive
+
+// Write a function called "isOldEnoughToDrinkAndDrive".
+
+// Given a number, in this case an age, "isOldEnoughToDrinkAndDrive" returns whether a person of this given age is old enough to legally drink and drive in the United States.
+
+// Notes:* The legal drinking age in the United States is 21.* It is always illegal to drink and drive in the United States.
+
+// var output = isOldEnoughToDrinkAndDrive(22);
+// console.log(output); // --> false
+
+    // SOLUTION:
+    // function isOldEnoughToDrinkAndDrive(age){
+    //     if(age >= 21){
+    //         return false
+    //     } else {
+    //         return false
+    //     }
+    // }
+    
+    // isOldEnoughToDrinkAndDrive(22);
+
+    // ===============================================>
+
+//    17)  Printing-Integers-While-notest
+
+// Printing Integers with for loop.
+
+// Print integers from 1000 to 3450.
+
+// SOLUTION:
+// for (let i = 1000; i<=3450; i++){
+    // console.log(i)
+//   };
+
+// =================================================>
+
+// 18) print-30-to-750-notest
+
+// Print numbers from -50 to 350.
+
+// Hint:* You can use for or while loop.
+
+// SOLUTION:
+// let i = -50;
+// while(i <= 350){
+//     console.log(i)
+//     i++
+// };
+
+// =====================================================>
+
+
+// 19)  Multiples-of-Three-notest
+
+// Using FOR loop, print multiples of 3 starting from -400 to 0. Skip numbers -3 and -6.
+
+// SOLUTION:
+// for( let i = -40; i <= 0; i++ ){
+//     if( i % 3 === 0 && i !== -3 && i !== -6 ){
+//           console.log(i)
+//     }
+// };
+
+// ==========================================================>
+
+// 20) Print-and-Count-notest
+
+// Print and Count
+
+// Using For loop, print all multiples of 5 starting from 512 to 4096. Count how many integers are multiples of 5 and log the amount on the console.
+
+// SOLUTION:
+// function printCount() {
+//     for (i = 512; i <= 4096; i++){
+//       console.log(i)
+//     }
+//   };
