5ff0dc1ebc064008ba46dc077104ce46a74d95af
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-02-26 18:43:38 +0100

    third part of the easy-coding-practice

---
+++ js-coding-practice-easy/script_3_.js
@@ -0,0 +1,57 @@
+//  21) checkAge
+
+// Write a function called "checkAge".
+
+// Given a person's name and age, "checkAge" returns one of two messages:
+
+// "Go home, {insert_name_here}!", if they are younger than 21."Welcome, {insert_name_here}!", if they are 21 or older.
+
+// Naturally, replace "{insert_name_here}" with the given name. :)
+
+// var output = checkAge('Adrian', 22);
+// console.log(output); // --> 'Welcome, Adrian!'
+
+// SOLUTION:
+// function checkAge(name, age) {
+//     if (age >= 21) {
+//       return `Welcome, ${name}!`
+//     } else {
+//       return `Go home, ${name}!`
+//     }
+//     }
+//     checkAge('Adrian', 22);
+
+// =====================================================>
+
+// 22)  Multiples-of-Six
+
+// Write a function called "multiplesOfSix".
+
+// Using WHILE loop, print multiples of 6 starting from 0 up to 60,000
+
+// var output = multiplesOfSix();
+// console.log(output); // --> 
+
+// SOLUTION:
+// function multiples0fSix(){
+//     for (i=0; i<=60 ; i++)
+//     console.log(i)
+//   } multiples0fSix()
+ 
+
+// =======================================================>
+
+
+// 23)  add-odd-integers-notest
+
+// Using FOR loop, Add odd integers from -300,000 to 300,000 and console.log the final sum.
+
+// var output = addOddIntegers();
+// console.log(output); 
+
+// SOLUTION:
+// for( let i = -3000 ; i<=3000; i++){
+//     console.log(i)
+//   }
+  
+  
