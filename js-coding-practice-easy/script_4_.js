b0456a4cef6952e4eadf60adad23e06b254fa40b
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-02-28 10:55:08 +0100

    daily dose

---
+++ js-coding-practice-easy/script_4_.js
@@ -0,0 +1,61 @@
+// 31) isOdd
+
+// Write a function called "isOdd".
+
+// Given a number, "isOdd" returns whether the given number is odd.
+
+// var output = isOdd(9);
+// console.log(output); // --> true
+
+// SOLUTION:
+// function isOdd(num) {
+//     if(num % 3===0){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+
+// =====================================================================>
+
+// 32) isSameLength
+
+// Write a function called "isSameLength".
+
+// Given two words, "isSameLength" returns whether the given words have the same length.
+
+// var output = isSameLength('words', 'super');
+// console.log(output); // --> true
+
+// SOLUTION:
+// function isSameLength(word1, word2) {
+//     if (word1.length === word2.length){
+//       return true
+//     } else{
+//       return false
+//     }
+//     }
+    
+//      isSameLength ("words", "super");
+
+// ==============================================================>
+
+// 33) areBothOdd
+
+// Write a function called "areBothOdd".
+
+// Given 2 numbers, "areBothOdd" returns whether or not both of the given numbers are odd.
+
+// var output = areBothOdd(1, 3);
+// console.log(output); // --> true
+
+
+// SOLUTION:
+// function areBothOdd(num1, num2) {
+//     if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+//       return true
+//     } else {
+//       return false
+//     }
+//     }
+//     areBothOdd(1,3);
