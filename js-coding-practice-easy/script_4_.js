6af5b401a0bee0d92956341e05e1a71561d8244a
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-02-28 11:00:25 +0100

    daily dose

--- js-coding-practice-easy/script_4_.js
+++ js-coding-practice-easy/script_4_.js
@@ -59,3 +59,72 @@
 //     }
 //     }
 //     areBothOdd(1,3);
+
+// ===================================================================>
+
+// 34) isEitherEven
+
+// Write a function called "isEitherEven".
+
+// Given two numbers, "isEitherEven" returns whether or not either one of the given numbers is even.
+
+// var output = isEitherEven(1, 4);
+// console.log(output); // --> true
+
+// SOLUTION:
+// function isEitherEven(num1, num2) {
+//     if(num1 % 2 === 0 || num2 % 2 === 0){
+//       return true
+//     } else {
+//       return false
+//     }
+    
+//     }
+//     isEitherEven(1, 4) 
+    
+
+// =================================================================>
+
+// 35) isOddLength
+
+// Write a function called "isOddLength".
+
+// Given a word, "isOddLength" returns whether the length of the given word is odd.
+
+// var output = isOddLength('special');
+// console.log(output); // --> true
+
+
+// SOLUTION:
+// function isOddLength(word) {
+ //     if (word.length % 2 !== 0){
+//       return true
+ //     } else {
+  //       return false
+ //     }
+//   }
+ //   isOddLength ('special');
+            
+            
+ // ===========================================================>
+
+// 36)  isEvenLength
+
+//  Write a function called "isEvenLength".
+            
+// Given a word, "isEvenLength" returns whether the length of the word is even.
+            
+// var output = isEvenLength('wow');
+//  console.log(output); // --> false
+
+// SOLUTION:
+// function isEvenLength(word) {
+//     if(word.length % 2 === 0){
+//       return true
+//     } else{
+//       return false
+//     }
+//   }
+
+// ===============================================================>
+
