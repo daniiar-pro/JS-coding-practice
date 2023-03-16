ceb5fc2c0fd263a73953fc89d375876ccb211e99
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-03-16 09:24:50 +0100

    daily dose

---
+++ js-coding-practice-junior/script_5_.js
@@ -0,0 +1,78 @@
+// 41) printAverage0fArray
+
+// Analyze an array's values and print the average.
+
+// var arr = [1,2,3,4]
+// `printAverage0fArray(arr) // 2.5`
+
+// SOLUTION:
+// // function printAverage0fArray(arr){
+
+// // let sum = 0;
+
+// // let av = 0;
+
+// // for(let i = 0; i < arr.length; i++){
+// //     sum += arr[i];
+// //     av = sum / arr.length;
+    
+// // }
+// // return av;
+// // }
+
+
+// function printAverage0fArray(arr){
+//     let result = arr[0];
+
+//     for(let i = 1; i < arr.length; i++){
+//         result += arr[i];
+//     }
+//     return result/ arr.length
+// };
+
+// ===================================================================>
+
+// 42) returnOdds
+
+// Return Odds from an Array
+
+// returnOdds([1,2,3,4]) // [1,3]
+
+// SOLUTION:
+// // function returnOdds(arr){
+// // const odds = arr.filter(ar => ar % 2 !== 0);
+// // return odds;
+// // }
+
+
+// function returnOdds(arr){
+//     let result = [];
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] % 2 !== 0){
+//             result.push(arr[i])
+//         }
+//     }
+//     return result;
+// };
+
+// ==========================================================>
+
+// 43) countGreaterThanY
+
+// Return Array Count Greater than Y.
+
+// Given an array and a value Y. count and print the number of array values greater than Y.
+
+// `countGreaterThanY([1,2,3], 2) //1`
+// `countGreaterThanY([2,2,2], 0) //3`
+
+// SOLUTION:
+// function countGreaterThanY(arr, y){
+//  let count = 0;
+
+//  for(let i = 0; i < arr.length; i++){
+//      if(arr[i] >y) count++
+//  }
+//  return count;
+// }
