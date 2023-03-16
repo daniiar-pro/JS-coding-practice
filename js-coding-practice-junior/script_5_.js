5a706bb67c02bef6b7fbd373b6614c33de29a0c8
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-03-16 09:29:44 +0100

    daily dose

--- js-coding-practice-junior/script_5_.js
+++ js-coding-practice-junior/script_5_.js
@@ -75,4 +75,67 @@
 //      if(arr[i] >y) count++
 //  }
 //  return count;
+// };
+
+// ===================================================================>
+
+// 44) printMax
+
+// Given an array, print the max.
+
+// `printMax([1,-12,3,1,0]) //3`
+// `printMax([2,2,2]) //2`
+
+// // function printMax(arr){
+// //     return Math.max(...arr)
+// // }
+
+// function printMax(arr){
+//     let max = 0;
+
+//     for(let i = 1; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+
+// =======================================================>
+
+// 45) scaleArr
+
+// Given array arr and number num, multiply each arr value by num, and return the changed arr.
+
+// `scale([2,3,4],5) // [10,15,20]`
+//  SOLUTION:
+// // function scaleArr(arr,num){
+// //  const sc = arr.map(ar => ar * num);
+// //  return sc;
+// // }
+
+
+// function scaleArr(arr, num){
+//     let result = [];
+
+//     for(let i = 0; i < arr.length; i++){
+//          arr[i] * num;
+//          result.push(arr[i] * num);
+//     }
+// return result 
+// };
+
+// =================================================================>
+
+// 46) reverse
+
+// Reverse Array.
+
+// Given array, write a function to reverse values in-place.
+
+// `reverse([34,6,4,2]) // [2,4,6,34]`
+
+// SOLUTION:
+// function reverse(arr) {
+ 
 // }
