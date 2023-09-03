116251bff891b4b06ced84afed6c21008d6b6425
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2023-09-04 01:09:11 +0200

    daily dose

--- js-coding-practice-medium/script_4_.js
+++ js-coding-practice-medium/script_4_.js
@@ -100,3 +100,20 @@
 // B: Oh finally!
 // C: Woah some cool data Oh finally!
 // D: Oops didn't work Oh finally!
+
+// 31) What's the output?
+// const myPromise = Promise.resolve('Woah some cool data');
+
+// (async () => {
+//   try {
+//     console.log(await myPromise);
+//   } catch {
+//     throw new Error(`Oops didn't work`);
+//   } finally {
+//     console.log('Oh finally!');
+//   }
+// })();
+// A: Woah some cool data
+// B: Oh finally!
+// C: Woah some cool data Oh finally!
+// D: Oops didn't work Oh finally!
